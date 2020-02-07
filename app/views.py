e2bd544066bb3b3c8bda8d6a5922d324d92c5c3b
Latoya Rhoden <rhoden_latoya@yahoo.com>
2020-02-07 16:45:26 -0500

    added format date function

--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
+import datetime 
+
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
@@ -27,8 +29,9 @@ def about():
 
 @app.route('/profile')
 def profile():
-    """ """
-    return render_template('profile.html')
+    """Render the website's profile page"""
+    joineddate = format_date_joined()
+    return render_template('profile.html', date=joineddate)
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -57,6 +60,12 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+date_joined = datetime.date(2020, 2, 7) # a specific date 
+def format_date_joined(date_joined):
+    ## Format the date to return only month and year date
+    print "Joined " + date_joined.strftime("%B, %Y") 
 
 
 if __name__ == '__main__':
